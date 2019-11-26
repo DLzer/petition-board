ae8dd8cce29d3d32118e933c11e1308d8cf6e5c8
Dillon Lomnitzer <yodaesu@gmail.com>
2019-11-26 11:02:38 -0500

    Minor verbiage change

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <!-- Submission -->
         <div class="col-md-4" style="margin-bottom:25px;">
-            <h5><strong>Welcome to Petition Board</strong></h5>
+            <h5><strong>Welcome to the Petition Board</strong></h5>
             <p style="font-size:14px;">This is a public petition board. This board allows you and all other users to create submissions for improvments, feature requests, bugs and focus areas for a specific application. </p>
             <p style="font-size:14px;">To get started, click the Make a Submission button below and fill out the form to complete your first petition. From there you can watch the progress and upvote count for your petition on the home page. Good Luck!</p>
             <div style="text-align:center; font-size: 14px;">
