eeb4bc16e14a4a8177d4c05534fed4b61473ea5e
Dillon Lomnitzer <yodaesu@gmail.com>
2020-11-09 16:27:38 -0500

    FontAwesome Fix

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -10,7 +10,7 @@
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 <!-- FontAwesome -->
-<script src="https://kit.fontawesome.com/c523afcadc.js" crossorigin="anonymous"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/js/all.min.js') }}"></script>
 <!-- Styles -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <style>
